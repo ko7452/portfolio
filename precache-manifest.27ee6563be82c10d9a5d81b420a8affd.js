e5357274a0cfae30a8b3153f4593a06a32fc8b6a
KoYunHyuk <sksgur3217@gmail.com>
2020-10-17 14:47:51 +0900

    Updates

---
+++ precache-manifest.27ee6563be82c10d9a5d81b420a8affd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99690a90e2b1a266a57dec284f8954a1",
+    "url": "/portfolio-pages/index.html"
+  },
+  {
+    "revision": "91ad1c1b1cf78ab3922e",
+    "url": "/portfolio-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a46617966b536919ab2",
+    "url": "/portfolio-pages/static/js/2.68f56c50.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio-pages/static/js/2.68f56c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91ad1c1b1cf78ab3922e",
+    "url": "/portfolio-pages/static/js/main.750c8e40.chunk.js"
+  },
+  {
+    "revision": "44e8590f27071bb11341",
+    "url": "/portfolio-pages/static/js/runtime-main.2044b274.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
