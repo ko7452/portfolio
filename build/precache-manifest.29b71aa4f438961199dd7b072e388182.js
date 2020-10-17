a3e5824298e68964c7e2270fe214d679799b0ee0
KoYunHyuk <sksgur3217@gmail.com>
2020-10-17 16:02:50 +0900

    first commit

---
+++ build/precache-manifest.29b71aa4f438961199dd7b072e388182.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09845d46da2b892cb625c8502727e4f8",
+    "url": "/portfolio-pages/index.html"
+  },
+  {
+    "revision": "3f1d90a68881efe063f9",
+    "url": "/portfolio-pages/static/css/main.3cdf6009.chunk.css"
+  },
+  {
+    "revision": "65f509d56914b4aea48c",
+    "url": "/portfolio-pages/static/js/2.7463ea8f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio-pages/static/js/2.7463ea8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f1d90a68881efe063f9",
+    "url": "/portfolio-pages/static/js/main.ad1e3ba0.chunk.js"
+  },
+  {
+    "revision": "44e8590f27071bb11341",
+    "url": "/portfolio-pages/static/js/runtime-main.2044b274.js"
+  },
+  {
+    "revision": "2908207289506a26337c6bf28669b988",
+    "url": "/portfolio-pages/static/media/Photo.29082072.png"
+  },
+  {
+    "revision": "af07b1f9072fb1dbc9298614863741e7",
+    "url": "/portfolio-pages/static/media/suits.af07b1f9.png"
+  }
+]);
