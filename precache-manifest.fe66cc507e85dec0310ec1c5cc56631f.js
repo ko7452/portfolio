b2f284bae78c04028c487c08444e3be3f11cf191
KoYunHyuk <sksgur3217@gmail.com>
2020-10-17 16:04:43 +0900

    Updates

---
+++ precache-manifest.fe66cc507e85dec0310ec1c5cc56631f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e64f0104a64914997c74b4d701d5c6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "95f320855cb3c90069eb",
+    "url": "/portfolio/static/css/main.2dabba66.chunk.css"
+  },
+  {
+    "revision": "65f509d56914b4aea48c",
+    "url": "/portfolio/static/js/2.7463ea8f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio/static/js/2.7463ea8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95f320855cb3c90069eb",
+    "url": "/portfolio/static/js/main.ad1e3ba0.chunk.js"
+  },
+  {
+    "revision": "b37b5bb5a98282a2f0e7",
+    "url": "/portfolio/static/js/runtime-main.b449097c.js"
+  },
+  {
+    "revision": "2908207289506a26337c6bf28669b988",
+    "url": "/portfolio/static/media/Photo.29082072.png"
+  },
+  {
+    "revision": "af07b1f9072fb1dbc9298614863741e7",
+    "url": "/portfolio/static/media/suits.af07b1f9.png"
+  }
+]);
