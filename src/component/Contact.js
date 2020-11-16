3c1e69b56a5d9365912da99d821a090861af716e
KoYunHyuk <sksgur3217@gmail.com>
2020-11-16 16:01:42 +0900

    completed contact

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,16 +1,55 @@
 import React from "react";
 import "./Contact.css";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Form, Col, Row } from "react-bootstrap";
+
 const Contact = () => {
   // 기능 구현 로직
   return (
     <div id="contact">
       <div class="Contact">
         <h1>Contact</h1>
-        <div class="contact-box-wraper">
-          <div class="contact-box">test</div>
-          <div class="contact-box">test</div>
-        </div>
+      </div>
+      <div class="Contact02">
+        <Form>
+          <Form.Group as={Row} controlId="formPlaintextEmail">
+            <Form.Label column sm="2">
+              Email:
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control
+                plaintext
+                readOnly
+                defaultValue="sksgur3217@gmail.com"
+              />
+            </Col>
+          </Form.Group>
+        </Form>
+        <Form>
+          <Form.Group as={Row} controlId="formPlaintextEmail">
+            <Form.Label column sm="2">
+              Github:
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control
+                plaintext
+                readOnly
+                defaultValue="https://github.com/ko7452"
+              />
+            </Col>
+          </Form.Group>
+        </Form>
+        <Form>
+          <Form.Group as={Row} controlId="formPlaintextEmail">
+            <Form.Label column sm="2">
+              Phone:
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control plaintext readOnly defaultValue="010-9800-5226" />
+            </Col>
+          </Form.Group>
+        </Form>
       </div>
     </div>
   );
